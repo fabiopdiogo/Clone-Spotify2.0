edc27bd2f0f51ac112cbfe8ec4cbc6f7330411ec
Fabio Diogo <fabindiogo@gmail.com>
2023-02-23 21:47:08 -0300

    pagina de login

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,28 +3,52 @@ import {
   SearchIcon,
   LibraryIcon,
   PlusCircleIcon,
+  HeartIcon,
+  RssIcon
 } from "@heroicons/react/outline"
 
 function Sidebar() {
   return (
-    <div>
-      <div>
-        <button type="">
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+      <div className="space-y-4">
+        <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5"/>
           <p>Home</p>
         </button>
-        <button type="">
+        <button className="flex items-center space-x-2 hover:text-white">
           <SearchIcon className="h-5 w-5"/>
-          <p>Home</p>
+          <p>Search</p>
         </button>
-        <button type="">
+        <button className="flex items-center space-x-2 hover:text-white">
           <LibraryIcon className="h-5 w-5"/>
-          <p>Home</p>
+          <p>Your Library</p>
         </button>
-        <button type="">
-          <PlusCircleIcon className="h-5 w-5"/>
-          <p>Home</p>
+
+        <hr className="border-t-[0.1] border-gray-900"/>
+
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HeartIcon className="h-5 w-5"/>
+          <p>Liked Songs</p>
         </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <LibraryIcon className="h-5 w-5"/>
+          <p>Your Library</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <RssIcon className="h-5 w-5"/>
+          <p>Your Episodes</p>
+        </button>
+        <hr className="border-t-[0.1] border-gray-900"/>
+
+        {/*Playlists...*/}
+        <p className="cursor-pointer hover:text-white"> Playlist name... </p>
+        <p className="cursor-pointer hover:text-white"> Playlist name... </p>
+        <p className="cursor-pointer hover:text-white"> Playlist name... </p>
+        <p className="cursor-pointer hover:text-white"> Playlist name... </p>
+        <p className="cursor-pointer hover:text-white"> Playlist name... </p>
+        <p className="cursor-pointer hover:text-white"> Playlist name... </p>
+        <p className="cursor-pointer hover:text-white"> Playlist name... </p>
+        <p className="cursor-pointer hover:text-white"> Playlist name... </p>
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import Head from 'next/head'
 import Sidebar from '../components/Sidebar'
 
 export default function Home (){
   return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Sidebar />
+    <div className="bg-black h-screen overflow-hidden">
+      <main>        
+          <Sidebar />
+          {/*Center*/}
+      </main>
+      
+      <div>{/*Player*/}</div>
     </div>
   )
 }
